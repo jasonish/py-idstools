c988166d8585045f9658e65e62ce62e36bb1f2ba
Jason Ish <ish@unx.ca>
2014-06-09 16:49:19 -0600

    Fix IPv6 address unpacking byte ordering.

--- idstools/unified2.py
+++ idstools/unified2.py
@@ -323,7 +323,7 @@ class EventDecoder(AbstractDecoder):
         if len(addr) == 4:
             return socket.inet_ntoa(addr)
         else:
-            parts = struct.unpack("H" * (len(addr) / 2), addr)
+            parts = struct.unpack(">" + "H" * (len(addr) / 2), addr)
             return ":".join("%x" % p for p in parts)
 
 class PacketDecoder(AbstractDecoder):
