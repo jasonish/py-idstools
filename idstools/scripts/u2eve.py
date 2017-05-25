jasonish/py-idstools

commit 8200f4cbbe1e2757e761f2da78abb913e4fd3d38

u2eve: don't flush incomplete event on exit
Changes tracking logic a bit...  An event will be flushed to disk
if one second goes by with no new events.  Extra packet records
are queued until a flush occurs so we don't flush an event
before its extra data is read.

idstools/scripts/u2eve.py
@@ -412,35 +412,49 @@ def main():
         return
 
     event = None
-    for record in reader:
-        if isinstance(record, unified2.Event):
-            if event is not None:
-                writer.write(event)
-                if args.bookmark and bookmark:
-                    location = reader.tell()
-                    bookmark.update(*location)
-            event = record
-        elif isinstance(record, unified2.ExtraData):
-            if not event:
-                continue
-            event["extra-data"].append(record)
-        elif isinstance(record, unified2.Packet):
-            if event and "packet" in event:
-                writer.write(event)
-                if args.bookmark and bookmark:
-                    location = reader.tell()
-                    bookmark.update(*location)
+    last_record_time = time.time()
+    queue = []
+
+    while True:
+        flush = False
+        record = reader._next()
+        if not record:
+            if event and time.time() - last_record_time > 1.0:
+                queue.append(event)
                 event = None
-            if event is None:
-                # Write packet...
-                writer.write(record)
-                if args.bookmark and bookmark:
-                    location = reader.tell()
-                    bookmark.update(*location)
+                flush = True
             else:
-                event["packet"] = record
-    if event:
-        writer.write(event)
+                time.sleep(0.01)
+        else:
+
+            last_record_time = time.time()
+
+            if isinstance(record, unified2.Event):
+                if event is not None:
+                    queue.append(event)
+                    flush = True
+                event = record
+            elif isinstance(record, unified2.ExtraData):
+                if not event:
+                    continue
+                event["extra-data"].append(record)
+            elif isinstance(record, unified2.Packet):
+                if not event:
+                    queue.append(record)
+                    flush = True
+                else:
+                    if "packet" in event:
+                        queue.append(record)
+                    else:
+                        event["packet"] = record
+
+        if flush:
+            for record in queue:
+                writer.write(record)
+            if args.bookmark and bookmark:
+                location = reader.tell()
+                bookmark.update(*location)
+            queue = []
 
 if __name__ == "__main__":
     sys.exit(main())

commit daa2dc4296c63d6ca6b8dbf262d5c8e3d315959c

u2eve: fix follow mode
issue introduced in commit
8200f4cbbe1e2757e761f2da78abb913e4fd3d38

idstools/scripts/u2eve.py
@@ -417,14 +417,21 @@ def main():
 
     while True:
         flush = False
-        record = reader._next()
+        record = reader.next()
+        done = False
         if not record:
             if event and time.time() - last_record_time > 1.0:
                 queue.append(event)
                 event = None
                 flush = True
             else:
-                time.sleep(0.01)
+                if args.follow:
+                    time.sleep(0.01)
+                else:
+                    if event:
+                        queue.append(event)
+                    flush = True
+                    done = True
         else:
 
             last_record_time = time.time()
@@ -455,6 +462,9 @@ def main():
                 location = reader.tell()
                 bookmark.update(*location)
             queue = []
+
+        if done:
+            break
 
 if __name__ == "__main__":
     sys.exit(main())

idstools/unified2.py
@@ -800,7 +800,7 @@ class SpoolRecordReader(object):
             return (self.fileobj.name, self.fileobj.tell())
         return None, None
 
-    def _next(self):
+    def next(self):
         """Return the next decoded unified2 record from the spool
         directory.
         """
@@ -831,7 +831,7 @@ class SpoolRecordReader(object):
                 else:
                     return None
 
-    def next(self):
+    def iter_next(self):
         """Return the next record or None if EOF.
 
         If in follow mode and EOF, this method will sleep and
@@ -843,7 +843,7 @@ class SpoolRecordReader(object):
 
         """
         while True:
-            record = self._next()
+            record = self.next()
             if record:
                 return record
             if not self.follow:
@@ -853,7 +853,7 @@ class SpoolRecordReader(object):
                 time.sleep(0.01)
 
     def __iter__(self):
-        return iter(self.next, None)
+        return iter(self.iter_next, None)
 
 class SpoolEventReader(object):
     """**Deprecated: Event readers have been deprecated due to the